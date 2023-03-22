3cab98688539e9d84747191226b9f377489f74bf
VirginieR <virginieruiz@gmail.com>
2023-03-22 10:59:27 -0400

    Bug fix in lock dic to manage access to the bam_file_info.dic, while recovering the primary read count of new bam files.

--- utils/primary_read_count.py
+++ utils/primary_read_count.py
@@ -29,31 +29,56 @@ class GetPrimaryReadCountBamFiles:
 		
 		return bam_file_info_to_return
 
+	def remove_lock_to_bam_files_info_dic(self, path_to_lock_file, path_to_input_folder):
+		with open(path_to_lock_file, 'r') as file:
+			first_line = file.readline().strip()
+		if first_line == path_to_input_folder:
+			first_line = ''
+			with open(path_to_lock_file, 'w') as file:
+				file.writelines(first_line) 
+			file.close()
+		else:
+			print ('Error in the path_to_lock_file: contents ', first_line, ' and the ', path_to_input_folder, ' not the same.')
+		
+
+	def grant_access_to_bam_files_info_dic(self, path_to_lock_file, path_to_input_folder, timeout=300):
+		start_time = time.time()
+		with open(path_to_lock_file, 'r') as file:
+			first_line = file.readline().strip()
+		while (time.time() - start_time < timeout) and first_line != path_to_input_folder:
+			try:
+				with open(path_to_lock_file, 'r') as file:
+					first_line = file.readline().strip()
+				if first_line == '':
+					with open(path_to_lock_file, 'w') as file:
+						file.write(path_to_input_folder)
+					file.close()
+					return True
+				time.sleep(1)
+			except IOError:
+				pass
+		raise Exception ("\nTimeout to grant access to Bam_files_info.dic. Another BamQuery process may be using the dictionary." )
+
 
 	def set_values(self, path_to_lib, bam_files_list, threads):
 
 		path_to_all_counts_file = path_to_lib+"Bam_files_info.dic"
-		
+		path_to_lock_file = path_to_lib+"lock_dic"
+
 		logging.info('Total Bam Files to Query : %d %s', len(bam_files_list), bam_files_list)
 		
+		if self.grant_access_to_bam_files_info_dic(path_to_lock_file, 'getting_primary_read_count_bam_files'):
+			logging.info('Lock access to Bam_files_info.dic')
+			pass
+		else:
+			raise Exception("\nCould not acquire the lock to consult the Bam_files_info.dic. Check if there is another run of BamQuery that wrote to the lock file and it is still running." )
+
 		if len(bam_files_list) >= threads:
 			pool = ProcessPool(nodes=threads)
 		else:
 			pool = ProcessPool(nodes=len(bam_files_list))
 		
 		results = pool.map(self.get_all_counts_bam_file, bam_files_list)
-
-		path_to_lock_file = path_to_lib+"lock_dic"
-		exists = os.path.exists(path_to_lock_file)
-
-		while exists:
-			exists = os.path.exists(path_to_lock_file)
-		
-		if not exists:
-			file_to_open = open(path_to_lock_file, 'w')
-			file_to_open.write('')
-			file_to_open.close()
-			logging.info('Lock Bam_files_info')
 
 		with open(path_to_all_counts_file, 'rb') as fp:
 			dictionary_total_reads_bam_files = pickle.load(fp)
@@ -68,7 +93,7 @@ class GetPrimaryReadCountBamFiles:
 
 		logging.info('Primary read counts for %s have been saved in the Bam_files_info.dic.', str(bam_files_list))
 
-		os.remove(path_to_lock_file)
+		self.remove_lock_to_bam_files_info_dic(path_to_lock_file, 'getting_primary_read_count_bam_files')
 		logging.info('Unlock Bam_files_info')
 
 		
